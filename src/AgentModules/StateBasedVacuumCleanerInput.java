2420b1563ab91bb277e2074e70cf3805d88873f4
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-06-07 11:58:29 -0400

    restructured directory

---
+++ src/AgentModules/StateBasedVacuumCleanerInput.java
@@ -0,0 +1,23 @@
+package AgentModules;
+
+import java.util.ArrayList;
+
+import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.inputs.StateBasedInput;
+import org.jLOAF.sim.SimilarityMetricStrategy;
+import org.jLOAF.sim.StateBasedSimilarity;
+
+public class StateBasedVacuumCleanerInput extends StateBasedInput{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public StateBasedVacuumCleanerInput(String name, StateBasedSimilarity sim) {
+		super(name, sim);	
+		//lol
+	}
+	
+
+}
---
+++ src/AgentModules/VacuumCleanerAgent.java
@@ -0,0 +1,59 @@
+package AgentModules;
+
+import org.jLOAF.Agent;
+import org.jLOAF.MotorControl;
+import org.jLOAF.Perception;
+import org.jLOAF.Reasoning;
+import org.jLOAF.action.Action;
+import org.jLOAF.action.AtomicAction;
+import org.jLOAF.casebase.CaseBase;
+import org.jLOAF.inputs.AtomicInput;
+import org.jLOAF.inputs.ComplexInput;
+import org.jLOAF.inputs.Input;
+import org.jLOAF.reasoning.BayesianReasoner;
+import org.jLOAF.reasoning.DynamicBayesianReasoner;
+import org.jLOAF.reasoning.SimpleKNN;
+import org.jLOAF.reasoning.TBReasoning;
+import org.jLOAF.sim.SimilarityMetricStrategy;
+import org.jLOAF.sim.atomic.Equality;
+import org.jLOAF.sim.complex.GreedyMunkrezMatching;
+import org.jLOAF.sim.complex.Mean;
+import org.jLOAF.sim.complex.WeightedMean;
+import org.jLOAF.weights.SimilarityWeights;
+
+/*
+ * VacuumCleanerAgent Object
+ * 
+ * 
+ * @author Ibrahim Ali Fawaz
+ * @since 2017 May
+ */
+public class VacuumCleanerAgent extends Agent{
+	
+	
+	/*
+	 * constructor 
+	 * 
+	 * @param casebase the casebase with all the cases the agent has observed.
+	 */
+	public VacuumCleanerAgent() {
+		super(null,null,null,null);
+		
+		this.mc = new VacuumCleanerMotorControl();
+		
+		this.p = new VacuumCleanerPerception();
+	
+	}
+	
+	
+
+	@Override
+	public Action run(Input input) {
+		AtomicAction a = (AtomicAction) this.r.selectAction(input);
+		return (VacuumCleanerAction) a;
+	}
+
+	
+	
+
+}
