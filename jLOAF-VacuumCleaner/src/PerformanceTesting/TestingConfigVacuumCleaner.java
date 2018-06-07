28fe4334124dbe1e3c4aa9d5164cd26b0e215241
Sacha Gunaratne <sacha.gunaratne@gmail.com>
2018-06-07 11:52:32 -0400

    Removed wierd merge diff thing in code.

--- jLOAF-VacuumCleaner/src/PerformanceTesting/TestingConfigVacuumCleaner.java
+++ jLOAF-VacuumCleaner/src/PerformanceTesting/TestingConfigVacuumCleaner.java
@@ -18,30 +18,19 @@ public class TestingConfigVacuumCleaner {
 		
 		
 		CaseBaseFilter ft =tc.createCaseBaseFilter(args);
-<<<<<<< HEAD
-<<<<<<< HEAD
-		//tc.changeSimilarity(args);
-=======
-		
-		
-		
->>>>>>> ec92234eb734353e24bef312dbd2c9e6be6a81eb
-=======
+
 			
->>>>>>> b885ed28f4fe9277625a84875b589520d4fb0a14
+
 		String r = tc.getReasoner(args);
 		String stSim =tc.getStSim(args);
 		String cpSim = tc.getCpSim(args);
-		
-<<<<<<< HEAD
-		try {
-			pt.PerformanceEvaluatorMethod(filenames, ft, output_filename,r,null,null);
-=======
+	
+
 		PerformanceTest pt = new PerformanceTest(); 
 		
 		try {
 			pt.PerformanceEvaluatorMethod(filenames, ft, output_filename,r,stSim,cpSim);
->>>>>>> ec92234eb734353e24bef312dbd2c9e6be6a81eb
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
